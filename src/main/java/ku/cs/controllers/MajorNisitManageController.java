4a34902f590e7e5657c7f640269d8c0ffd3d1529
Tonwantpillow <kerdsiri.s@ku.th>
2024-08-03 19:13:11 +0700

    Add Major role UI and styles

---
+++ src/main/java/ku/cs/controllers/MajorNisitManageController.java
@@ -0,0 +1,34 @@
+package ku.cs.controllers;
+
+import javafx.fxml.FXML;
+import ku.cs.services.FXRouter;
+
+import java.io.IOException;
+
+public class MajorNisitManageController {
+    @FXML
+    protected void onApproverManageButtonClick() {
+        try {
+            FXRouter.goTo("major-approver-manage");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @FXML
+    protected void onNisitManageButtonClick() {
+        try {
+            FXRouter.goTo("major-nisit-manage");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @FXML
+    protected void onAppealManageButtonClick() {
+        try {
+            FXRouter.goTo("major-appeal-manage");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
--- src/main/java/ku/cs/cs211671project/MainApplication.java
+++ src/main/java/ku/cs/cs211671project/MainApplication.java
@@ -11,14 +11,19 @@ import java.io.IOException;
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXRouter.bind(this, stage, "CS211 Project", 1024, 768);
+        FXRouter.bind(this, stage, "CS211 Project", 1440, 832);
         configRoutes();
-        FXRouter.goTo("hello");
+        FXRouter.goTo("major-appeal-manage");
     }
 
     private void configRoutes() {
         String viewPath = "ku/cs/views/";
         FXRouter.when("hello", viewPath + "hello-view.fxml");
+        FXRouter.when("major-appeal-manage", viewPath + "major-appeal-manage.fxml");
+        FXRouter.when("major-approver-manage", viewPath + "major-approver-manage.fxml");
+        FXRouter.when("major-nisit-manage", viewPath + "major-nisit-manage.fxml");
+
+
     }
 
     public static void main(String[] args) {
